ba00f1c5ae0b6f2cd01a274a6a8eed44437a2ffc
GeorgeJuniorGG <g216741@dac.unicamp.br>
2022-04-29 11:41:16 -0300

    Adicionando um loop no carrinho

--- projeto02/src/main.c
+++ projeto02/src/main.c
@@ -3,8 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <math.h>
 
-#define n 8
+#define n 10
 #define C 5
 
 typedef unsigned int ui;
@@ -14,58 +15,63 @@ sem_t mutex1, mutex2;
 sem_t boardQueue, unboardQueue;
 sem_t allAboard, allAshore;
 
-void* car()
+void *car()
 {
-    // load();
-    printf("O carro está pronto o embarque.\n");
+    int rounds = (int)ceil((double)n / (double)C);
 
-    for(ui i = 0; i < C; i++)
-        sem_post(&boardQueue);
+    for (int i = 0; i < rounds; i++)
+    {
+        // load();
+        printf("O carro está pronto o embarque.\n");
 
-    sem_wait(&allAboard);
+        for (ui i = 0; i < C; i++)
+            sem_post(&boardQueue);
 
-    // run();
-    printf("O carro está se movimentando.\n");
+        sem_wait(&allAboard);
 
-    // unload();
-    printf("O carro está pronto o desembarque.\n");
+        // run();
+        printf("O carro está se movimentando.\n");
 
-    for(ui i = 0; i < C; i++)
-        sem_post(&unboardQueue);
+        // unload();
+        printf("O carro está pronto o desembarque.\n");
 
-    sem_wait(&allAshore);
+        for (ui i = 0; i < C; i++)
+            sem_post(&unboardQueue);
+
+        sem_wait(&allAshore);
+    }
 
     return NULL;
 }
 
-void* passenger(void* p)
+void *passenger(void *p)
 {
-    ui id = *(ui*) p;
+    ui id = *(ui *)p;
 
     sem_wait(&boardQueue);
     // board();
 
     sem_wait(&mutex1);
-        boarders++;
-        printf("O passageiro %d embarcou.\n", id);
-        if(boarders == C)
-        {
-            sem_post(&allAboard);
-            boarders = 0;
-        }
+    boarders++;
+    printf("O passageiro %d embarcou.\n", id);
+    if (boarders == C)
+    {
+        sem_post(&allAboard);
+        boarders = 0;
+    }
     sem_post(&mutex1);
 
     sem_wait(&unboardQueue);
     // unboard();
 
     sem_wait(&mutex2);
-        unboarders++;
-        printf("O passageiro %d desembarcou.\n", id);
-        if(unboarders == C)
-        {
-            sem_post(&allAshore);
-            unboarders = 0;
-        }
+    unboarders++;
+    printf("O passageiro %d desembarcou.\n", id);
+    if (unboarders == C)
+    {
+        sem_post(&allAshore);
+        unboarders = 0;
+    }
     sem_post(&mutex2);
 
     return NULL;
@@ -83,18 +89,18 @@ int main(int argc, char *argv[])
     sem_init(&allAboard, 0, 0);
     sem_init(&allAshore, 0, 0);
 
-    for(ui i = 0; i < n; i++)
+    for (ui i = 0; i < n; i++)
     {
         id_psgr[i] = i;
-        pthread_create(&thr_psgrs[i], NULL, passenger, (void*) &id_psgr[i]);
+        pthread_create(&thr_psgrs[i], NULL, passenger, (void *)&id_psgr[i]);
     }
 
     pthread_create(&thr_car, NULL, car, NULL);
 
-    for(ui i = 0; i < n; i++)
+    for (ui i = 0; i < n; i++)
         pthread_join(thr_psgrs[i], NULL);
-    
+
     pthread_join(thr_car, NULL);
-    
+
     return 0;
 }
